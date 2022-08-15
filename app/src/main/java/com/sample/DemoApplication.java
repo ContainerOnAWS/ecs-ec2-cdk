683a08fdb59b32e277e935721bea9b7b7d45db18
engel80 <prettyhip@gmail.com>
2022-08-15 11:03:36 +0900

    .

--- app/src/main/java/com/sample/DemoApplication.java
+++ app/src/main/java/com/sample/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.sec.sample;
+package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- app/src/main/java/com/sample/SampleController.java
+++ app/src/main/java/com/sample/SampleController.java
@@ -1,4 +1,4 @@
-package com.sec.sample;
+package com.sample;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,21 +9,21 @@ public class SampleController {
 
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String home() {
-        return new String("OK");
+        return "OK";
     }
 
     @RequestMapping(value="/{seviceid}/{pathvar1}", method=RequestMethod.GET)
     public String pingPath2() {
-        return new String("OK");
+        return "OK";
     }
 
     @RequestMapping(value="/{seviceid}/{pathvar1}/{pathvar2}", method=RequestMethod.GET)
     public String pingPath3() {
-        return new String("OK");
+        return "OK";
     }
 
     @RequestMapping(value="/{seviceid}/{pathvar1}/{pathvar2}/{pathvar3}", method=RequestMethod.GET)
     public String pingPath4() {
-        return new String("OK");
+        return "OK";
     }
 }
