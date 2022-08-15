e0a5bd9b2ca9136dd569fec34581286126a3ed4a
engel80 <prettyhip@gmail.com>
2022-08-15 11:08:36 +0900

    .

--- ecs-restapi-service/lib/ecs-restapi-service-stack.ts
+++ ecs-restapi-service/lib/ecs-restapi-service-stack.ts
@@ -1,6 +1,6 @@
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
-import { Stack, StackProps, CfnOutput, Duration } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, Duration, Lazy, RemovalPolicy } from 'aws-cdk-lib';
 import * as path from 'path';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
@@ -41,8 +41,8 @@ export class EcsRestAPIServiceStack extends Stack {
         const taskDefinition = new ecs.TaskDefinition(this, 'task-definition', {
             compatibility: ecs.Compatibility.EC2,
             family: `${serviceName}-task`,
-            executionRole: iam.Role.fromRoleArn(this, 'task-execution-role', cdk.Lazy.string({ produce: () => executionRoleArn })),
-            taskRole: iam.Role.fromRoleArn(this, 'task-role', cdk.Lazy.string({ produce: () => taskRoleArn }))
+            executionRole: iam.Role.fromRoleArn(this, 'task-execution-role', Lazy.string({ produce: () => executionRoleArn })),
+            taskRole: iam.Role.fromRoleArn(this, 'task-role', Lazy.string({ produce: () => taskRoleArn }))
         });
         const container = taskDefinition.addContainer('container-restapi', {
             containerName,
@@ -75,7 +75,7 @@ export class EcsRestAPIServiceStack extends Stack {
             scaleInCooldown: Duration.seconds(120)
         });
         const logGroup = new logs.LogGroup(this, 'loggroup', {
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            removalPolicy: RemovalPolicy.DESTROY,
             logGroupName: serviceName,
             retention: logs.RetentionDays.TWO_WEEKS,
         });
@@ -95,7 +95,7 @@ export class EcsRestAPIServiceStack extends Stack {
             loadBalancerName: `alb-${serviceName}`,
             internetFacing: true,
             deletionProtection: false,
-            idleTimeout: cdk.Duration.seconds(30),
+            idleTimeout: Duration.seconds(30),
         });
 
         const listener = alb.addListener('https-listener', {
